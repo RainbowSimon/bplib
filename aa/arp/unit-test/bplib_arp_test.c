95c805e7b9d281cb85da8c73779fe7a536a2c7ab
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-10-22 15:35:00 +0000

    DTNN-1436: Write unit tests for ARP

--- aa/arp/unit-test/bplib_arp_test.c
+++ aa/arp/unit-test/bplib_arp_test.c
@@ -38,7 +38,7 @@ void Test_BPLib_ARP_CRSTblValidateFunc_Nominal(void)
     BPLib_ARP_CRSTable_t TestTblData;
     memset(&TestTblData, 0, sizeof(TestTblData));
     TestTblData.CRS_Set[0].SizeTrigger = 10;
-    UtAssert_INT32_EQ((int32) BPLib_ARP_CRSTblValidateFunc(&TestTblData), (int32) BPLIB_SUCCESS);     
+    UtAssert_INT32_EQ((int32) BPLib_ARP_CRSTblValidateFunc(&TestTblData), (int32) BPLIB_SUCCESS);
 }
 
 void Test_BPLib_ARP_CRSTblValidateFunc_Invalid(void)
@@ -49,13 +49,116 @@ void Test_BPLib_ARP_CRSTblValidateFunc_Invalid(void)
     /* Error case should return BPLIB_TABLE_OUT_OF_RANGE_ERR_CODE */
     TestTblData.CRS_Set[0].SizeTrigger = 0;
 
-    UtAssert_INT32_EQ(BPLib_ARP_CRSTblValidateFunc(&TestTblData), 
+    UtAssert_INT32_EQ(BPLib_ARP_CRSTblValidateFunc(&TestTblData),
                                                 BPLIB_TABLE_OUT_OF_RANGE_ERR_CODE);
+}
+
+void Test_BPLib_ARP_GetDispCodeIdx_Pos(void)
+{
+    UtAssert_EQ(BPLib_CT_DispositionCode_t, BPLib_ARP_GetDispCodeIdx(BPLib_CT_CustodyAccepted), BPLib_CT_CustodyAccepted_Idx);
+}
+
+void Test_BPLib_ARP_GetDispCodeIdx_Neg(void)
+{
+    UtAssert_EQ(BPLib_CT_DispositionCode_t, BPLib_ARP_GetDispCodeIdx(BPLib_CT_CustodyRefused), BPLib_CT_CustodyRefused_Idx);
+}
+
+void Test_BPLib_ARP_ProcessBsr(void)
+{
+    BPLib_Bundle_t    Bundle;
+    BPLib_MEM_Block_t Block;
+
+    memset(&Bundle, 0, sizeof(BPLib_Bundle_t));
+    memset(&Block, 0, sizeof(BPLib_MEM_Block_t));
+    Bundle.blob = &Block;
+
+    BPLib_ARP_ProcessBsr(&AdminRecord, &Bundle);
+
+    BPLib_ARP_Test_VerifyIncrement(BPLIB_EID_INSTANCE, BUNDLE_COUNT_RECEIVED_ADMIN_RECORD, 1, 1);
+}
+
+void Test_BPLib_ARP_ProcessCrs(void)
+{
+    BPLib_Bundle_t    Bundle;
+    BPLib_MEM_Block_t Block;
+
+    memset(&Bundle, 0, sizeof(BPLib_Bundle_t));
+    memset(&Block, 0, sizeof(BPLib_MEM_Block_t));
+    Bundle.blob = &Block;
+
+    BPLib_ARP_ProcessBsr(&AdminRecord, &Bundle);
+
+    BPLib_ARP_Test_VerifyIncrement(BPLIB_EID_INSTANCE, BUNDLE_COUNT_RECEIVED_ADMIN_RECORD, 1, 1);
+}
+
+void Test_BPLib_ARP_ProcessCcs(void)
+{
+    BPLib_Bundle_t          Bundle;
+    BPLib_MEM_Block_t       Block;
+    BPLib_ARP_AdminRecord_t BlobAsAdminRecord;
+    uint8_t                 BundleSeqIdx;
+    uint8_t                 SeqRangeIdx;
+
+    memset(&Bundle, 0, sizeof(BPLib_Bundle_t));
+    memset(&Block, 0, sizeof(BPLib_MEM_Block_t));
+    Bundle.blob = &Block;
+
+    BPLib_ARP_ProcessCcs(&AdminRecord, &Bundle);
+
+    BPLib_ARP_Test_VerifyIncrement(BPLIB_EID_INSTANCE, BUNDLE_COUNT_RECEIVED_ADMIN_RECORD,   1, 1);
+    BPLib_ARP_Test_VerifyIncrement(BPLIB_EID_INSTANCE, BUNDLE_COUNT_RECEIVED_CUSTODY_SIGNAL, 1, 2);
+
+    UtAssert_STUB_COUNT(BPLib_AS_Increment, 2);
+
+    BlobAsAdminRecord = *((BPLib_ARP_AdminRecord_t*) Bundle.blob->user_data.raw_bytes);
+
+    UtAssert_EQ(BPLib_ARP_AdminRecordTypeCode_t,
+                BlobAsAdminRecord.AdminRecordType,
+                AdminRecord.AdminRecordType);
+
+    for (BundleSeqIdx = 0; BundleSeqIdx < BPLIB_CT_MAX_SEQ_COLLECTIONS; BundleSeqIdx++)
+    {
+        UtAssert_EQ(uint64_t,
+                    BlobAsAdminRecord.BundleSeqCollections[BundleSeqIdx].SeqId,
+                    AdminRecord.BundleSeqCollections[BundleSeqIdx].SeqId);
+
+        UtAssert_EQ(uint64_t,
+                    BlobAsAdminRecord.BundleSeqCollections[BundleSeqIdx].FirstSeqNum,
+                    AdminRecord.BundleSeqCollections[BundleSeqIdx].FirstSeqNum);
+
+        for (SeqRangeIdx = 0; SeqRangeIdx < BPLIB_CT_MAX_SEQ_RANGE_LEN; SeqRangeIdx++)
+        {
+            UtAssert_EQ(uint64_t,
+                        BlobAsAdminRecord.BundleSeqCollections[BundleSeqIdx].SeqRange[SeqRangeIdx],
+                        AdminRecord.BundleSeqCollections[BundleSeqIdx].SeqRange[SeqRangeIdx]);
+        }
+
+        UtAssert_EQ(size_t,
+                    BlobAsAdminRecord.BundleSeqCollections[BundleSeqIdx].SeqRangeLen,
+                    AdminRecord.BundleSeqCollections[BundleSeqIdx].SeqRangeLen);
+
+        UtAssert_EQ(size_t,
+                    BlobAsAdminRecord.BundleSeqCollections[BundleSeqIdx].LastSeqNumAdded,
+                    AdminRecord.BundleSeqCollections[BundleSeqIdx].LastSeqNumAdded);
+
+        UtAssert_EQ(BPLib_CT_DispositionCode_t,
+                    BlobAsAdminRecord.BundleSeqCollections[BundleSeqIdx].DispositionCode,
+                    AdminRecord.BundleSeqCollections[BundleSeqIdx].DispositionCode);
+    }
+
+    UtAssert_EQ(size_t,
+                BlobAsAdminRecord.NumSeqCollections,
+                AdminRecord.NumSeqCollections);
 }
 
 void TestBplibArp_Register(void)
 {
-    UtTest_Add(Test_BPLib_ARP_Init, BPLib_ARP_Test_Setup, BPLib_ARP_Test_Teardown, "Test_BPLib_ARP_Init");
-    UtTest_Add(Test_BPLib_ARP_CRSTblValidateFunc_Nominal, BPLib_ARP_Test_Setup, BPLib_ARP_Test_Teardown, "Test_BPLib_ARP_CRSTblValidateFunc_Nominal");
-    UtTest_Add(Test_BPLib_ARP_CRSTblValidateFunc_Invalid, BPLib_ARP_Test_Setup, BPLib_ARP_Test_Teardown, "Test_BPLib_ARP_CRSTblValidateFunc_Invalid");
+    ADD_TEST(Test_BPLib_ARP_Init);
+    ADD_TEST(Test_BPLib_ARP_CRSTblValidateFunc_Nominal);
+    ADD_TEST(Test_BPLib_ARP_CRSTblValidateFunc_Invalid);
+    ADD_TEST(Test_BPLib_ARP_GetDispCodeIdx_Pos);
+    ADD_TEST(Test_BPLib_ARP_GetDispCodeIdx_Neg);
+    ADD_TEST(Test_BPLib_ARP_ProcessBsr);
+    ADD_TEST(Test_BPLib_ARP_ProcessCrs);
+    ADD_TEST(Test_BPLib_ARP_ProcessCcs);
 }
--- aa/arp/unit-test/utilities/bplib_arp_test_utils.c
+++ aa/arp/unit-test/utilities/bplib_arp_test_utils.c
@@ -18,20 +18,69 @@
  *
  */
 
-/*
-** Include
-*/
+/* ======== */
+/* Includes */
+/* ======== */
 
 #include "bplib_arp_test_utils.h"
+#include "bplib_as_handlers.h"
+#include "bplib_mem.h"
 
-/*
-** Function Definitions
-*/
+/* ======= */
+/* Globals */
+/* ======= */
+
+BPLib_ARP_AdminRecord_t AdminRecord;
+
+/* ==================== */
+/* Function Definitions */
+/* ==================== */
+
+void BPLib_ARP_Test_VerifyIncrement(BPLib_EID_t EID, BPLib_AS_Counter_t Counter, uint32_t Amount, int16_t CallNum)
+{
+    UtAssert_EQ(uint64_t, EID.Scheme,       Context_BPLib_AS_Increment[CallNum - 1].EID.Scheme);
+    UtAssert_EQ(uint64_t, EID.IpnSspFormat, Context_BPLib_AS_Increment[CallNum - 1].EID.IpnSspFormat);
+    UtAssert_EQ(uint64_t, EID.Allocator,    Context_BPLib_AS_Increment[CallNum - 1].EID.Allocator);
+    UtAssert_EQ(uint64_t, EID.Node,         Context_BPLib_AS_Increment[CallNum - 1].EID.Node);
+    UtAssert_EQ(uint64_t, EID.Service,      Context_BPLib_AS_Increment[CallNum - 1].EID.Service);
+
+    if (Counter != -1)
+    {
+        UtAssert_EQ(BPLib_AS_Counter_t, Counter, Context_BPLib_AS_Increment[CallNum - 1].Counter);
+    }
+
+    if (Amount != 0xFFFFFFFF)
+    {
+        UtAssert_EQ(uint32_t, Amount, Context_BPLib_AS_Increment[CallNum - 1].Amount);
+    }
+}
 
 void BPLib_ARP_Test_Setup(void)
 {
     /* Initialize test environment to default state for every test */
     UT_ResetState(0);
+
+    AdminRecord.AdminRecordType = BPLib_CT_CcsRecordTypeCode;
+
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].SeqId           = 1;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].FirstSeqNum     = 12;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].SeqRange[0]     = 2;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].SeqRange[1]     = 3;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].SeqRange[2]     = 4;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].SeqRange[3]     = 5;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].SeqRangeLen     = 4;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].LastSeqNumAdded = 5;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyAccepted_Idx].DispositionCode = BPLib_CT_CustodyAccepted;
+
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyRefused_Idx].SeqId           = 1;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyRefused_Idx].FirstSeqNum     = 12;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyRefused_Idx].SeqRange[0]     = 2;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyRefused_Idx].SeqRange[1]     = 3;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyRefused_Idx].SeqRangeLen     = 2;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyRefused_Idx].LastSeqNumAdded = 3;
+    AdminRecord.BundleSeqCollections[BPLib_CT_CustodyRefused_Idx].DispositionCode = BPLib_CT_CustodyRefused;
+
+    UT_SetHandlerFunction(UT_KEY(BPLib_AS_Increment), UT_Handler_BPLib_AS_Increment, NULL);
 }
 
 void BPLib_ARP_Test_Teardown(void)
