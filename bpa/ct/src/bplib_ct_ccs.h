37edbf741943c5f3b9c02865e73855eaa7473a6a
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-11-20 16:32:57 +0000

    DTNN-1435: Add a bit more to build and send open CCS function

--- bpa/ct/src/bplib_ct_ccs.h
+++ bpa/ct/src/bplib_ct_ccs.h
@@ -65,7 +65,7 @@ size_t BPLib_CT_GetOpenCcsIdx(BPLib_CT_Context_t *Context, BPLib_MEM_Pool_t* Poo
 /**
  * \brief     After an open CCS has hit the size, time, or open CCS limit, this
  *            function will send the CCS and memory pool off to ARP to build the
- *            CCS into a bundle
+ *            CCS into a bundle. The open CCS is then reset for continued use
  * \param[in] OpenCcs The open CCS that will be wrapped in a bundle
  * \param[in] Pool    The memory pool from which the bundle with a CCS payload
  *                    will be created
