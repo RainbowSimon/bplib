fcbe5fc302009371112b4cf97d530999fc674ae9
Cameron Sykes <cameron.j.sykes@nasa.gov>
2026-02-13 19:24:59 +0000

    DTNN-1535: Update event message

--- bpa/ct/src/bplib_ct.c
+++ bpa/ct/src/bplib_ct.c
@@ -130,8 +130,8 @@ BPLib_Status_t BPLib_CT_ProcessNewBundle(BPLib_Instance_t* Inst, BPLib_Bundle_t 
     if ((Inst->BundleStorage.BytesStorageInUse + Bundle->Meta.TotalBytes + sizeof(BPLib_BBlocks_t)) >= BPLIB_MAX_STORED_BUNDLE_BYTES)
     {
         BPLib_EM_SendEvent(BPLIB_CT_NO_STOR_ERR_EID, BPLib_EM_EventType_ERROR,
-                            "Cannot accept bundle, not enough storage remaining (%ld bytes). %s.",
-                            Inst->BundleStorage.BytesStorageInUse, BundleInfo);
+                            "Cannot accept bundle, not enough storage (%ld bytes remaining). %s.",
+                            (BPLIB_MAX_STORED_BUNDLE_BYTES - Inst->BundleStorage.BytesStorageInUse), BundleInfo);
         BPLib_AS_Increment(BPLIB_EID_INSTANCE, BUNDLE_COUNT_DEPLETED, 1);
         Status = BPLIB_CT_CUSTODY_REFUSED_ERR;
     }
--- bpa/stor/src/bplib_stor.c
+++ bpa/stor/src/bplib_stor.c
@@ -126,8 +126,8 @@ BPLib_Status_t BPLib_STOR_StoreBundle(BPLib_Instance_t* Inst, BPLib_Bundle_t* Bu
     if ((Inst->BundleStorage.BytesStorageInUse + Bundle->Meta.TotalBytes + sizeof(BPLib_BBlocks_t)) >= BPLIB_MAX_STORED_BUNDLE_BYTES)
     {
         BPLib_EM_SendEvent(BPLIB_CT_NO_STOR_ERR_EID, BPLib_EM_EventType_ERROR,
-                            "Cannot accept bundle, not enough storage remaining (%ld bytes).",
-                            Inst->BundleStorage.BytesStorageInUse);
+                            "Cannot accept bundle, not enough storage (%ld bytes remaining).",
+                            (BPLIB_MAX_STORED_BUNDLE_BYTES - Inst->BundleStorage.BytesStorageInUse));
         BPLib_AS_Increment(BPLIB_EID_INSTANCE, BUNDLE_COUNT_DELETED_NO_STORAGE, 1);
         BPLib_AS_Increment(BPLIB_EID_INSTANCE, BUNDLE_COUNT_DELETED, 1);
         BPLib_AS_Increment(BPLIB_EID_INSTANCE, BUNDLE_COUNT_DISCARDED, 1);
