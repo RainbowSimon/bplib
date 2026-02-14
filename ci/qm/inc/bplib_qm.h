e28a9825c1bbe0156062cba778bd90ba9f885047
Simon Grund <simon.grund@tuhh.de>
2026-02-14 02:28:18 +0100

    Fix encoding issue with no additional CRC
    CBORD size 0x86 (6 array elements) was hard-coded, but when no CRC is
    used, there is no sixth element, leading to invalid CBOR (which also
    bplib could not decode).

--- ci/qm/inc/bplib_qm.h
+++ ci/qm/inc/bplib_qm.h
@@ -32,7 +32,7 @@
 
 #define QM_NO_WAIT          0L  /**< Constant representing no wait */
 #define QM_WAIT_FOREVER    -1L /**< Constant representing an indefinite wait */
-#define QM_MAX_GEN_WORKERS  8L /**< Constant representing maximum allowed generic workers */
+#define QM_MAX_GEN_WORKERS  1L /**< Constant representing maximum allowed generic workers */
 
 #ifndef BPLIB_QM_TX_QUEUE_DEPTH
 #define BPLIB_QM_TX_QUEUE_DEPTH 2048
